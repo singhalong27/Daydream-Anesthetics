ef132d272cb5a2c24f200e4c2ac361fdfa2b143a
singhalong27 <singhalong27@gmail.com>
2025-07-27 17:37:53 -0500

    Delete daydream-anesthetics 2 directory

--- daydream-anesthetics 2/components/ui/button.js
+++
@@ -1,7 +0,0 @@
-export function Button({ className = '', children, ...props }) {
-  return (
-    <button className={`rounded-lg px-4 py-2 font-semibold ${className}`} {...props}>
-      {children}
-    </button>
-  );
-}
--- daydream-anesthetics 2/components/ui/card.js
+++
@@ -1,7 +0,0 @@
-export function Card({ className = '', children }) {
-  return <div className={`bg-white ${className}`}>{children}</div>;
-}
-
-export function CardContent({ className = '', children }) {
-  return <div className={`${className}`}>{children}</div>;
-}
